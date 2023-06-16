6209cb9a2317a8d10d9b9fa072a2ceeb03c1d051
GG1GG <>
2023-06-16 16:42:16 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Напишите два числа для, програма выедет какое большее, а какое меньшее.");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a > b)  
+    Console.WriteLine($"Число {a}  больше числа {b} ");
+else
+    Console.WriteLine($"Число {b}  больше числа {a} ");
